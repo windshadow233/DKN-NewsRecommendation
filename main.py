97e73b6368b631c358746bd41221d08db32d0bc6
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-12 22:46:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from torch.optim import Adam
 from torch.utils.data import DataLoader
+import tqdm
 from data.dataset import WordsTokenConverter, UserDataset, user_data_collate
 from model.DKN import *
 from config import config
 
-model = DKN(config, "data/title_entities_embedding.pkl")
+device = torch.device('cuda:0')
+model = DKN(config, "data/title_entities_embedding.pkl").to(device)
 converter1 = WordsTokenConverter()
 converter2 = WordsTokenConverter("data/title_entities_vocab.json")
 dataset = UserDataset(converter1, converter2)
+loss_fcn = nn.BCEWithLogitsLoss()
+optimizer = Adam(model.parameters(), lr=2e-4, betas=(0.5, 0.99))
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=user_data_collate)
+for candidate_news, clicked_news, is_click in tqdm.tqdm(dataloader, total=len(dataloader)):
+    optimizer.zero_grad()
+    candidate_news = candidate_news.to(device)
+    clicked_news = clicked_news.to(device)
+    is_click = is_click.to(device)
+    pred = model(candidate_news, clicked_news, False)
+    loss = loss_fcn(pred, is_click)
+    print(loss.item())
+    loss.backward()
+    optimizer.step()
+
