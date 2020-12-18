d39fe4cfd2e68a58fb5c845f878c978eed75214f
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-18 15:09:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 import tqdm
 import random
-from data.dataset import WordsTokenConverter, UserDataset, user_data_collate
+from data.dataset import WordsTokenConverter, TrainDataset, user_data_collate, TestDataset
 from model.DKN import *
 from config import config
 
@@ -12,10 +12,10 @@ device = torch.device(config.device)
 model = DKN(config, "data/entities_embedding.pkl").to(device)
 converter1 = WordsTokenConverter('data/title_words_vocab.json')
 converter2 = WordsTokenConverter("data/entities_vocab.json")
-train_set = UserDataset(converter1, converter2, positive_rate=0.5)
+train_set = TrainDataset(converter1, converter2, positive_rate=0.5)
 loss_fcn = nn.BCEWithLogitsLoss()
 optimizer = Adam(model.parameters(), lr=2e-4, betas=(0.5, 0.98))
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True, collate_fn=user_data_collate)
+train_loader = DataLoader(train_set, batch_size=64, shuffle=False, collate_fn=user_data_collate)
 epochs = 5
 total_step = len(train_loader)
 for epoch in range(epochs):
