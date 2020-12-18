4ea956172bc3f57332b6d3f47b59d36f9b13cec5
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2020-12-18 15:08:56 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -8,8 +8,10 @@ from data.utils import *
 # os.chdir('..')
 import json
 import time
+import tqdm
 from data.utils import *
 from config import config
+
 device = torch.device(config.device)
 
 
@@ -105,26 +107,23 @@ class WordsTokenConverter:
         return ' '.join([self.idx2char.get(token) for token in tokens if token != config.pad_idx])
 
 
-class UserDataset(Dataset):
+class TrainDataset(Dataset):
     def __init__(self,
                  title_converter: WordsTokenConverter,
                  entity_converter: WordsTokenConverter,
-                 category_dict='data/categories.json',
-                 behaviors='data/train/behaviors.tsv',
-                 news='data/train/news.tsv',
                  positive_rate=0.3):
         self.title_converter = title_converter
         self.entity_converter = entity_converter
-        with open(category_dict, 'r') as f:
+        with open('data/categories.json', 'r') as f:
             self.category_dict = json.loads(f.read())
         print('Loading data...')
-        self.behaviors = pd.read_csv(behaviors, sep='\t', header=None).set_index(0)
+        self.behaviors = pd.read_csv('data/train/behaviors.tsv', sep='\t', header=None).set_index(0)
         self.behaviors.index.name = 'Impression_ID'
         self.behaviors.columns = ['User_ID', 'Time', 'History', 'Impressions']
         self.users_id = self.behaviors.User_ID.unique().tolist()
         # 先按User_ID分组,以加快读取速度
         self.behaviors = self.behaviors.groupby(by='User_ID')
-        self.news = pd.read_csv(news, sep='\t', header=None, index_col=0)
+        self.news = pd.read_csv('data/train/news.tsv', sep='\t', header=None, index_col=0)
         self.news.index.name = 'News_ID'
         self.news.columns = ['Category', 'SubCategory', 'Title', 'Abstract', 'URL', 'Title_Entities',
                              'Abstract_Entities']
@@ -158,7 +157,7 @@ class UserDataset(Dataset):
         for i, behavior in behaviors.iterrows():
             history = behavior.History
             if not pd.isna(history):
-                click_history.extend(behavior.History.split(' '))
+                click_history.extend(history.split(' '))
             impressions.extend(behavior.Impressions.split(' '))
         ####################### candidate news #######################
         # 以positive_rate概率抽取正例
@@ -210,3 +209,78 @@ class UserDataset(Dataset):
 
     def __len__(self):
         return len(self.users_id)
+
+
+class TestDataset(object):
+    def __init__(self,
+                 title_converter: WordsTokenConverter,
+                 entity_converter: WordsTokenConverter,
+                 mode='test'):
+        self.title_converter = title_converter
+        self.entity_converter = entity_converter
+        with open('data/categories.json', 'r') as f:
+            self.category_dict = json.loads(f.read())
+        print('Loading data...')
+        self.behaviors = pd.read_csv(f'data/{mode}/behaviors.tsv', sep='\t', header=None).set_index(0)
+        self.behaviors.index.name = 'Impression_ID'
+        self.behaviors.columns = ['User_ID', 'Time', 'History', 'Impressions']
+        self.users_id = self.behaviors.User_ID.unique().tolist()
+        self.users = self.behaviors.groupby(by='User_ID')
+        self.news = pd.read_csv(f'data/{mode}/news.tsv', sep='\t', header=None, index_col=0)
+        self.news.index.name = 'News_ID'
+        self.news.columns = ['Category', 'SubCategory', 'Title', 'Abstract', 'URL', 'Title_Entities',
+                             'Abstract_Entities']
+        self.mode = mode
+        print('Finish!')
+
+    def __len__(self):
+        return len(self.behaviors)
+
+    def __getitem__(self, item):
+        behavior = self.behaviors.iloc[item]
+        user_id = behavior.User_ID
+        behaviors = self.users.get_group(user_id)
+        click_history = []
+        impressions = []
+        for i, line in behaviors.iterrows():
+            history = line.History
+            if not pd.isna(history):
+                click_history.extend(history.split(' '))
+        impressions.extend(behavior.Impressions.split(' '))
+        clicked_news = []
+        for history in click_history[-config.num_clicked_news_per_user:]:
+            to_add = {}
+            news = self.news.loc[history]
+            split_title = split_words(news.Title)
+            entities = news.Title_Entities
+            entities = [] if pd.isna(entities) else json.loads(entities)
+            entities = get_entities_from_title(split_title, entities)
+            to_add['title'] = self.title_converter.wors2token(split_title)
+            to_add['entities'] = self.entity_converter.wors2token(entities)
+            to_add['category'] = torch.tensor(self.category_dict.get(news.Category, 0)).long()
+            clicked_news.append(to_add)
+        for impression in impressions:
+            if self.mode == 'test':
+                candidate_id = impression
+                is_click = torch.tensor(0.5)
+            else:
+                candidate_id, is_click = impression.split('-')
+                is_click = torch.tensor(int(is_click)).float()
+            candidate = self.news.loc[candidate_id]
+            split_title = split_words(candidate.Title)
+            candidate_category = torch.tensor(self.category_dict.get(candidate.Category, 0)).long()
+            candidate_title = self.title_converter.wors2token(split_title)
+            entities = candidate.Title_Entities
+            entities = [] if pd.isna(entities) else json.loads(entities)
+            entities = get_entities_from_title(split_title, entities)
+            candidate_title_entities = self.entity_converter.wors2token(entities)
+            to_return = {
+                'candidate_news': {
+                    'title': candidate_title,
+                    'entities': candidate_title_entities,
+                    'is_click': is_click,
+                    'category': candidate_category
+                },
+                'clicked_news': clicked_news
+            }
+            yield to_return
