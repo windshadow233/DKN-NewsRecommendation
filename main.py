af10499692ce0254ba6fe9ec416984b957970972
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-02 02:04:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ torch.manual_seed(10)
 random.seed(10)
 device = torch.device(config.device)
 model = DKN(config, "data/entities_embedding.pkl").to(device)
-train_set = TrainDataset('data/title_words_vocab.json', 'data/entities_vocab.json', positive_rate=0.5)
+train_set = TrainDataset('data/title_words_vocab.json', 'data/entities_vocab.json', positive_rate=0.167)
 loss_fcn = nn.BCEWithLogitsLoss()
-optimizer = Adam(model.parameters(), lr=2e-4, betas=(0.5, 0.98))
+optimizer = Adam(model.parameters(), lr=1e-4, betas=(0.5, 0.98))
 train_loader = DataLoader(train_set, batch_size=64, shuffle=False, collate_fn=user_data_collate)
-epochs = 5
+epochs = 10
 total_step = len(train_loader)
 for epoch in range(epochs):
     for candidate_news, clicked_news, is_click in tqdm.tqdm(train_loader,
