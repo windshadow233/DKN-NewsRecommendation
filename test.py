634c4a400501b9bd54b7c85a0d8c43d15c5206f5
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-04 01:45:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,9 +15,12 @@ model.eval()
 test_dataset = TestDataset('data/title_words_vocab.json', 'data/entities_vocab.json', mode='test')
 with torch.no_grad():
     with open('prediction.txt', 'w') as f:
-        for data in tqdm.tqdm(tqdm.tqdm(test_dataset)):
-            impression_ID = next(data)
-            candidate, history, _ = user_data_collate(data)
-            pred = model(candidate, history).tolist()
-            rank = pd.Series(pred).rank(ascending=False).astype(int).to_list()
-            f.write(f'{impression_ID} {str(rank)}\n')
+        try:
+            for data in tqdm.tqdm(test_dataset):
+                impression_ID = next(data)
+                candidate, history, _ = user_data_collate(data)
+                pred = model(candidate, history).tolist()
+                rank = pd.Series(pred).rank(ascending=False).astype(int).to_list()
+                f.write(f'{impression_ID} {str(rank)}\n')
+        except IndexError:
+            pass
