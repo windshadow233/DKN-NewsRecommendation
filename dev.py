39efd4440aec0e2e1f50cc74f765786c850732f6
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-04 00:37:49 +0800

    Create dev.py

---
+++ dev.py
@@ -0,0 +1,26 @@
+import torch
+import tqdm
+import os
+from sklearn.metrics import roc_auc_score
+from model.DKN import DKN
+from config import Config
+from data.dataset import TestDataset, user_data_collate
+
+
+model_path = 'trained_model/model2'
+config = Config(os.path.join(model_path, 'config.json'))
+model = DKN(config, 'data/entities_embedding.pkl')
+model.load_state_dict(torch.load(os.path.join(model_path, 'state_dict', 'model.pkl')))
+model.eval()
+dev_dataset = TestDataset('data/title_words_vocab.json', 'data/entities_vocab.json', mode='dev')
+auc = 0
+with torch.no_grad():
+    for i, data in enumerate(tqdm.tqdm(dev_dataset), 1):
+        next(data)
+        candidate, history, truth = user_data_collate(data)
+        truth = truth.cpu().numpy()
+        pred = model(candidate, history).cpu().numpy()
+        auc_score = roc_auc_score(truth, pred)
+        auc += auc_score
+        print(f'AUC: {auc_score} | Avg: {auc / i}')
+
