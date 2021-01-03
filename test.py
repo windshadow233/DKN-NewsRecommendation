842c9bfd032ce2e46b35a8fa8fbc69d0e011e8a5
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-04 00:37:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import torch
 import tqdm
-from sklearn.metrics import roc_auc_score
+import os
+import pandas as pd
 from model.DKN import DKN
 from config import Config
 from data.dataset import TestDataset, user_data_collate
 
 
-config = Config('trained_model/model1/config.json')
+model_path = 'trained_model/model2'
+config = Config(os.path.join(model_path, 'config.json'))
 model = DKN(config, 'data/entities_embedding.pkl')
-model.load_state_dict(torch.load('trained_model/model1/state_dict/model.pkl'))
+model.load_state_dict(torch.load(os.path.join(model_path, 'state_dict', 'model.pkl')))
 model.eval()
-dev_dataset = TestDataset('data/title_words_vocab.json', 'data/entities_vocab.json', mode='dev')
-auc = 0
+test_dataset = TestDataset('data/title_words_vocab.json', 'data/entities_vocab.json', mode='test')
 with torch.no_grad():
-    for i, data in enumerate(tqdm.tqdm(dev_dataset), 1):
-        candidate, history, truth = user_data_collate(data)
-        truth = truth.cpu().numpy()
-        pred = model(candidate, history).cpu().numpy()
-        auc_score = roc_auc_score(truth, pred)
-        auc += auc_score
-        print(f'AUC: {auc_score} | Avg: {auc / i}')
-
+    with open('prediction.txt', 'w') as f:
+        for data in tqdm.tqdm(tqdm.tqdm(test_dataset)):
+            impression_ID = next(data)
+            candidate, history, _ = user_data_collate(data)
+            pred = model(candidate, history).tolist()
+            rank = pd.Series(pred).rank(ascending=False).astype(int).to_list()
+            f.write(f'{impression_ID} {str(rank)}\n')
